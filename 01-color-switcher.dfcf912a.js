313a3eeed51d9d2bb9d44540801f33062972eea1
Olgadz469 <133228372+Olgadz469@users.noreply.github.com>
2023-09-20 15:27:34 +0000

    Deploying to gh-pages from @ Olgadz469/goit-js-hw-09@2e228841afae785641d2fb150c87d4ee49ececb4 🚀

---
+++ 01-color-switcher.dfcf912a.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop]");t.addEventListener("click",(function(){n=setInterval(o,1e3),t.disabled=!0})),e.addEventListener("click",(function(){clearInterval(n),t.disabled=!1,console.log(`Interval with id ${n} has stopped!`)}));let n=null;function o(){const t=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`;document.body.style.backgroundColor=t}
+//# sourceMappingURL=01-color-switcher.dfcf912a.js.map
