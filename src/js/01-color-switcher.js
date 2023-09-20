2e228841afae785641d2fb150c87d4ee49ececb4
Olgadz469 <133228372+Olgadz469@users.noreply.github.com>
2023-09-20 18:27:07 +0300

    hw-js-09

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,47 @@
 
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+startBtn.addEventListener("click", onClickStart);
+stopBtn.addEventListener("click", onClickStop);
+let timerId = null;
+
+function onClickStart () {
+
+  timerId = setInterval(changeBackgroundColor, 1000);
+  startBtn.disabled = true;
+}
+
+function onClickStop () {
+
+    clearInterval(timerId);
+    startBtn.disabled = false;
+    console.log(`Interval with id ${timerId} has stopped!`);
+}
+
+function changeBackgroundColor() {
+
+    const randomColor = getRandomHexColor();
+    document.body.style.backgroundColor = randomColor;
+}
+
+function getRandomHexColor() {
+
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
+
+
+// HTML містить кнопки «Start» і «Stop».
+
+// <button type="button" data-start>Start</button>
+// <button type="button" data-stop>Stop</button>
+
+// Напиши скрипт, який після натискання кнопки «Start»,
+// раз на секунду змінює колір фону <body> на випадкове значення,
+// використовуючи інлайн стиль. Натисканням на кнопку «Stop» зміна
+// кольору фону повинна зупинятися.
+
+// УВАГА
+// Враховуй, що на кнопку «Start» можна натиснути нескінченну
+// кількість разів. Зроби так, щоб доки зміна теми запущена,
+// кнопка «Start» була неактивною (disabled).
+
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,94 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import Notiflix from 'notiflix';
+
+const form = document.querySelector('.form');
+
+form.addEventListener('submit', onSubmit)
+
+function onSubmit(e) {
+  e.preventDefault();
+
+  const delay = Number(e.currentTarget.delay.value);
+  const step = Number(e.currentTarget.step.value);
+  const amount = Number(e.currentTarget.amount.value);
+
+  for (let i = 1; i <= amount; i++) {
+      createPromise(i, delay + (i - 1) * step);
   }
 }
+
+function createPromise(position, delay) {
+  const promise = new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+
+  promise.then(({ position, delay }) => {
+      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    }).catch(({ position, delay }) => {
+      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+}
+
+
+// HTML містить розмітку форми, в поля якої користувач буде вводити
+// першу затримку в мілісекундах, крок збільшення затримки для кожного
+// промісу після першого і кількість промісів, яку необхідно створити.
+
+// <form class="form">
+//   <label>
+//     First delay (ms)
+//     <input type="number" name="delay" required />
+//   </label>
+//   <label>
+//     Delay step (ms)
+//     <input type="number" name="step" required />
+//   </label>
+//   <label>
+//     Amount
+//     <input type="number" name="amount" required />
+//   </label>
+//   <button type="submit">Create promises</button>
+// </form>
+
+// Напиши скрипт, який на момент сабміту форми викликає функцію
+// createPromise(position, delay) стільки разів, скільки ввели
+// в поле amount. Під час кожного виклику передай їй номер
+// промісу (position), що створюється, і затримку, враховуючи першу
+// затримку (delay), введену користувачем, і крок (step).
+
+// function createPromise(position, delay) {
+//   const shouldResolve = Math.random() > 0.3;
+//   if (shouldResolve) {
+//     // Fulfill
+//   } else {
+//     // Reject
+//   }
+// }
+
+// Доповни код функції createPromise таким чином, щоб вона повертала
+// один проміс, який виконується або відхиляється через delay часу.
+// Значенням промісу повинен бути об'єкт, в якому будуть властивості
+// position і delay зі значеннями однойменних параметрів. Використовуй
+// початковий код функції для вибору того, що потрібно зробити з промісом -
+// виконати або відхилити.
+
+// createPromise(2, 1500)
+//   .then(({ position, delay }) => {
+//     console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+//   })
+//   .catch(({ position, delay }) => {
+//     console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+//   });
+
+// Бібліотека повідомлень
+// УВАГА
+// Наступний функціонал не обов'язковий для здавання завдання, але буде хорошою додатковою практикою.
+
+// Для відображення повідомлень користувачеві, замість console.log(), використовуй бібліотеку notiflix.
